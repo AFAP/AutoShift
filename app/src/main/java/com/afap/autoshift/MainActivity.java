66bd44b791be0b37d0244088583fda981905ce0d
AFAP_PC <814237762@qq.com>
2017-07-25 22:52:58 +0800

    V1.0.2

--- app/src/main/java/com/afap/autoshift/MainActivity.java
+++ app/src/main/java/com/afap/autoshift/MainActivity.java
@@ -10,7 +10,6 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.afap.autoshift.model.Ask;
 import com.afap.autoshift.model.Coin;
 import com.afap.autoshift.model.Depth;
 import com.afap.autoshift.model.DepthOrder;
@@ -19,7 +18,6 @@ import com.afap.autoshift.net.BaseSubscriber;
 import com.afap.autoshift.net.Network;
 import com.afap.autoshift.utils.LogUtil;
 import com.afap.utils.ToastUtil;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.text.DecimalFormat;
@@ -32,7 +30,7 @@ import rx.schedulers.Schedulers;
 public class MainActivity extends AppCompatActivity {
 
 
-    private final static int LIMIT_SIZE = 10; // 买卖各询问10个单子
+    private final static int LIMIT_SIZE = 6; // 买卖各询问10个单子
     private DecimalFormat df_rate = new DecimalFormat("0.0000000000");
     private DecimalFormat df = new DecimalFormat("0.00000");
 
@@ -40,13 +38,23 @@ public class MainActivity extends AppCompatActivity {
 
     private Spinner spinner_coin1, spinner_coin2;
 
-    private EditText et_amount_a, et_amount_b;
+    private EditText et_amount_a_sc, et_amount_b_eth;
     private TextView info_sc_sell, info_sc_buy_cost;
     private TextView info_eth_buy, info_eth_sell_earn;
     private TextView rate_sc_eth, sc_eth_overview;
 
-    private double amount_a, avage_a, total_pay = 0;
-    private double amount_b, avage_b, total_earn = 0;
+    private double amount_a_sc, avage_a_sc, total_pay_sc = 0;
+    private double amount_b_eth, avage_b_eth, total_earn_eth = 0;
+    /**
+     * SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS
+     **/
+    private EditText et_amount_a_eth, et_amount_b_sc;
+    private TextView info_eth_sell, info_eth_buy_cost;
+    private TextView info_sc_buy, info_sc_sell_earn;
+    private TextView rate_eth_sc, eth_sc_overview;
+
+    private double amount_a_eth, avage_a_eth, total_pay_eth = 0;
+    private double amount_b_sc, avage_b_sc, total_earn_sc = 0;
 
 
     @Override
@@ -56,8 +64,8 @@ public class MainActivity extends AppCompatActivity {
 
         initData();
 
-        et_amount_a = (EditText) findViewById(R.id.amount_a);
-        et_amount_b = (EditText) findViewById(R.id.amount_b);
+        et_amount_a_sc = (EditText) findViewById(R.id.amount_a_sc);
+        et_amount_b_eth = (EditText) findViewById(R.id.amount_b_eth);
 
         info_sc_sell = (TextView) findViewById(R.id.info_sc_sell);
         info_sc_buy_cost = (TextView) findViewById(R.id.info_sc_buy_cost);
@@ -65,6 +73,18 @@ public class MainActivity extends AppCompatActivity {
         info_eth_sell_earn = (TextView) findViewById(R.id.info_eth_sell_earn);
         rate_sc_eth = (TextView) findViewById(R.id.rate_sc_eth);
         sc_eth_overview = (TextView) findViewById(R.id.sc_eth_overview);
+
+        //**************************************
+
+        et_amount_a_eth = (EditText) findViewById(R.id.amount_a_eth);
+        et_amount_b_sc = (EditText) findViewById(R.id.amount_b_sc);
+
+        info_eth_sell = (TextView) findViewById(R.id.info_eth_sell);
+        info_eth_buy_cost = (TextView) findViewById(R.id.info_eth_buy_cost);
+        info_sc_buy = (TextView) findViewById(R.id.info_sc_buy);
+        info_sc_sell_earn = (TextView) findViewById(R.id.info_sc_sell_earn);
+        rate_eth_sc = (TextView) findViewById(R.id.rate_eth_sc);
+        eth_sc_overview = (TextView) findViewById(R.id.eth_sc_overview);
 
 
         spinner_coin1 = (Spinner) findViewById(R.id.spinner_coin1);
@@ -74,14 +94,26 @@ public class MainActivity extends AppCompatActivity {
         spinner_coin2.setAdapter(new MyAdapter());
 
 
-        findViewById(R.id.btn_test).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_test1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                amount_a = Double.parseDouble(et_amount_a.getText().toString());
-                if (amount_a > 0) {
-                    total_pay = 0;
-                    total_earn = 0;
-                    getMarketInfo();
+                amount_a_sc = Double.parseDouble(et_amount_a_sc.getText().toString());
+                if (amount_a_sc > 0) {
+                    total_pay_sc = 0;
+                    total_earn_eth = 0;
+                    getMarketInfo_SC_ETH();
+                }
+            }
+        });
+
+        findViewById(R.id.btn_test2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                amount_a_eth = Double.parseDouble(et_amount_a_eth.getText().toString());
+                if (amount_a_eth > 0) {
+                    total_pay_eth = 0;
+                    total_earn_sc = 0;
+                    getMarketInfo_ETH_SC();
                 }
             }
         });
@@ -97,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void getMarketInfo() {
+    private void getMarketInfo_SC_ETH() {
         Network
                 .getShapeShiftAPIService()
                 .getMarketInfo("SC_ETH")
@@ -109,19 +141,19 @@ public class MainActivity extends AppCompatActivity {
                         LogUtil.i("SC_ETH", jsonObject.toString());
 
                         ShiftInfo shiftInfo = ShiftInfo.parseFromJson(jsonObject);
-                        if (shiftInfo.getLimit() < amount_a) {
+                        if (shiftInfo.getLimit() < amount_a_sc) {
                             ToastUtil.showShort("待转换数量超出限制");
                         } else {
-                            amount_b = shiftInfo.getRate() * amount_a - shiftInfo.getMinerFee();
-                            et_amount_b.setText(df.format(amount_b));
-                            rate_sc_eth.setText("当前比率:" + df_rate.format( shiftInfo.getRate()));
-                            getADepth("");
+                            amount_b_eth = shiftInfo.getRate() * amount_a_sc - shiftInfo.getMinerFee();
+                            et_amount_b_eth.setText(df.format(amount_b_eth));
+                            rate_sc_eth.setText("当前比率:" + df_rate.format(shiftInfo.getRate()));
+                            getADepth_SC();
                         }
                     }
                 });
     }
 
-    private void getADepth(String type) {
+    private void getADepth_SC() {
         Network
                 .getYunbiAPIService()
                 .getDepth("sccny", LIMIT_SIZE)
@@ -144,15 +176,15 @@ public class MainActivity extends AppCompatActivity {
                             double amount = asks.get(i).getAmount();
                             String flag = "";
 
-                            gap_amount = amount_a - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
+                            gap_amount = amount_a_sc - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
                             total_amount = total_amount + amount; // 至当前深度，可买入的数量
                             if (gap_amount <= 0) { //  至上一前深度，已经满足
 
-                            } else if (total_amount >= amount_a) { // 至当前深度，已经满足需要买入的数量
-                                total_pay = total_pay + gap_amount * price;
+                            } else if (total_amount >= amount_a_sc) { // 至当前深度，已经满足需要买入的数量
+                                total_pay_sc = total_pay_sc + gap_amount * price;
                                 flag = "**";
                             } else {
-                                total_pay = total_pay + amount * price;
+                                total_pay_sc = total_pay_sc + amount * price;
                             }
 
 
@@ -161,16 +193,17 @@ public class MainActivity extends AppCompatActivity {
                             }
                             sb.append(df.format(price)).append("  ").append(amount).append(flag);
                         }
-                        avage_a = total_pay / amount_a;
+                        avage_a_sc = total_pay_sc / amount_a_sc;
 
                         info_sc_sell.setText(sb.toString());
-                        info_sc_buy_cost.setText(String.format(getString(R.string.cost_average), df.format(avage_a)));
-                        getBDepth("");
+                        info_sc_buy_cost.setText(String.format(getString(R.string.cost_average), df.format
+                                (avage_a_sc)));
+                        getBDepth_ETH();
                     }
                 });
     }
 
-    private void getBDepth(String type) {
+    private void getBDepth_ETH() {
         Network
                 .getYunbiAPIService()
                 .getDepth("ethcny", LIMIT_SIZE)
@@ -193,15 +226,15 @@ public class MainActivity extends AppCompatActivity {
                             double amount = bids.get(i).getAmount();
                             String flag = "";
 
-                            gap_amount = amount_b - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
+                            gap_amount = amount_b_eth - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
                             total_amount = total_amount + amount; // 至当前深度，可买入的数量
                             if (gap_amount <= 0) { //  至上一前深度，已经满足
 
-                            } else if (total_amount >= amount_b) { // 至当前深度，已经满足需要买入的数量
-                                total_earn = total_earn + gap_amount * price;
+                            } else if (total_amount >= amount_b_eth) { // 至当前深度，已经满足需要买入的数量
+                                total_earn_eth = total_earn_eth + gap_amount * price;
                                 flag = "**";
                             } else {
-                                total_earn = total_earn + amount * price;
+                                total_earn_eth = total_earn_eth + amount * price;
                             }
 
 
@@ -210,14 +243,146 @@ public class MainActivity extends AppCompatActivity {
                             }
                             sb.append(df.format(price)).append("  ").append(amount).append(flag);
                         }
-                        avage_b = total_earn / amount_b;
+                        avage_b_eth = total_earn_eth / amount_b_eth;
 
                         info_eth_buy.setText(sb.toString());
-                        info_eth_sell_earn.setText(String.format(getString(R.string.earn_average), df.format(avage_b)));
+                        info_eth_sell_earn.setText(String.format(getString(R.string.earn_average), df.format
+                                (avage_b_eth)));
 
-                        String overview = String.format("买入A花费%f,卖出B得到%f,获利：%f", total_pay, total_earn, (total_earn -
-                                total_pay));
+                        String overview = String.format("买入A花费%f,卖出B得到%f,获利：%f", total_pay_sc, total_earn_eth,
+                                (total_earn_eth -
+                                        total_pay_sc));
                         sc_eth_overview.setText("++" + overview);
+
+                    }
+                });
+    }
+
+
+    private void getMarketInfo_ETH_SC() {
+        Network
+                .getShapeShiftAPIService()
+                .getMarketInfo("ETH_SC")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseSubscriber<JsonObject>() {
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        LogUtil.i("ETH_SC", jsonObject.toString());
+
+                        ShiftInfo shiftInfo = ShiftInfo.parseFromJson(jsonObject);
+                        if (shiftInfo.getLimit() < amount_a_eth) {
+                            ToastUtil.showShort("待转换数量超出限制");
+                        } else {
+                            amount_b_sc = shiftInfo.getRate() * amount_a_eth - shiftInfo.getMinerFee();
+                            et_amount_b_sc.setText(df.format(amount_b_sc));
+                            rate_eth_sc.setText("当前比率:" + df_rate.format(shiftInfo.getRate()));
+                            getADepth_ETH();
+                        }
+                    }
+                });
+    }
+
+    private void getADepth_ETH() {
+        Network
+                .getYunbiAPIService()
+                .getDepth("ethcny", LIMIT_SIZE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseSubscriber<JsonObject>() {
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        LogUtil.i("getADepth", jsonObject.toString());
+
+                        double total_amount = 0;
+                        double gap_amount = 0; // 深度缺口
+
+                        Depth depth = Depth.parseFromJson(jsonObject);
+                        List<DepthOrder> asks = depth.getAsks();
+
+                        StringBuffer sb = new StringBuffer();
+                        for (int i = 0; i < asks.size(); i++) {
+                            double price = asks.get(i).getPrice();
+                            double amount = asks.get(i).getAmount();
+                            String flag = "";
+
+                            gap_amount = amount_a_eth - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
+                            total_amount = total_amount + amount; // 至当前深度，可买入的数量
+                            if (gap_amount <= 0) { //  至上一前深度，已经满足
+
+                            } else if (total_amount >= amount_a_eth) { // 至当前深度，已经满足需要买入的数量
+                                total_pay_eth = total_pay_eth + gap_amount * price;
+                                flag = "**";
+                            } else {
+                                total_pay_eth = total_pay_eth + amount * price;
+                            }
+
+
+                            if (i > 0) {
+                                sb.append("\n");
+                            }
+                            sb.append(df.format(price)).append("  ").append(amount).append(flag);
+                        }
+                        avage_a_eth = total_pay_eth / amount_a_eth;
+
+                        info_eth_sell.setText(sb.toString());
+                        info_eth_buy_cost.setText(String.format(getString(R.string.cost_average), df.format
+                                (avage_a_eth)));
+                        getBDepth_SC();
+                    }
+                });
+    }
+
+    private void getBDepth_SC() {
+        Network
+                .getYunbiAPIService()
+                .getDepth("sccny", LIMIT_SIZE)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseSubscriber<JsonObject>() {
+                    @Override
+                    public void onNext(JsonObject jsonObject) {
+                        LogUtil.i("getBDepth_SC", jsonObject.toString());
+
+                        double total_amount = 0;
+                        double gap_amount = 0; // 深度缺口
+
+                        Depth depth = Depth.parseFromJson(jsonObject);
+                        List<DepthOrder> bids = depth.getBids();
+
+                        StringBuffer sb = new StringBuffer();
+                        for (int i = 0; i < bids.size(); i++) {
+                            double price = bids.get(i).getPrice();
+                            double amount = bids.get(i).getAmount();
+                            String flag = "";
+
+                            gap_amount = amount_b_sc - total_amount; // 至上一前深度，要满足买入数量还需要的缺口
+                            total_amount = total_amount + amount; // 至当前深度，可买入的数量
+                            if (gap_amount <= 0) { //  至上一前深度，已经满足
+
+                            } else if (total_amount >= amount_b_sc) { // 至当前深度，已经满足需要买入的数量
+                                total_earn_sc = total_earn_sc + gap_amount * price;
+                                flag = "**";
+                            } else {
+                                total_earn_sc = total_earn_sc + amount * price;
+                            }
+
+
+                            if (i > 0) {
+                                sb.append("\n");
+                            }
+                            sb.append(df.format(price)).append("  ").append(amount).append(flag);
+                        }
+                        avage_b_sc = total_earn_sc / amount_b_sc;
+
+                        info_sc_buy.setText(sb.toString());
+                        info_sc_sell_earn.setText(String.format(getString(R.string.earn_average), df.format
+                                (avage_b_sc)));
+
+                        String overview = String.format("买入A花费%f,卖出B得到%f,获利：%f", total_pay_eth, total_earn_sc,
+                                (total_earn_sc -
+                                        total_pay_eth));
+                        eth_sc_overview.setText("++" + overview);
 
                     }
                 });
