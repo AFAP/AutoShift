62880c4f7835f2ae1cd45ea967c5ed89b3551e7e
AFAP <814237762@qq.com>
2017-12-15 15:00:39 +0800

    火币API请求时加上头

--- app/src/main/java/com/afap/autoshift/net/HuobiService.java
+++ app/src/main/java/com/afap/autoshift/net/HuobiService.java
@@ -3,6 +3,7 @@ package com.afap.autoshift.net;
 import com.google.gson.JsonObject;
 
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -13,6 +14,13 @@ public interface HuobiService {
      * 获取指定市场的买卖深度
      */
     // https://api.huobi.pro/market/depth?symbol=zecusdt&type=step0
+    @Headers({
+            "User-Agent: Mozilla/5.0 (Linux; Android 7.0; ZUK Z2131 Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/55.0.2883.91 Mobile Safari/537.36",
+            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Content-Type: application/json",
+            "Accept-Language: zh-CN"
+
+    })
     @GET("/market/depth?type=step0")
     Observable<JsonObject> getDepth(@Query("symbol") String symbol);
 
